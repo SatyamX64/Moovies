e60e3fc3d32c3c37e0c1cb65a8f6c94d926e3b6a
satyamx64 <satyamx64@gmail.com>
2021-07-29 23:21:12 +0530

    [HOME] add toolbar

--- lib/presentation/common_widgets/widget_spacer.dart
+++ lib/presentation/common_widgets/widget_spacer.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 class WidgetSpacer extends StatelessWidget {
   late final double _height;
+  late final double _width;
 
-  WidgetSpacer({double? height}) : _height = height ?? 16;
+  WidgetSpacer({double? height, double? width})
+      : _height = height ?? 16,
+        _width = width ?? 0;
 
   @override
   Widget build(BuildContext context) {
-    return Divider(
-      color: Colors.transparent,
+    return Container(
       height: _height,
+      width: _width,
     );
   }
 }
