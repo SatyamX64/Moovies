cd93122df6be9f9ee8dbb3319d35d4946c27344f
satyamx64 <satyamx64@gmail.com>
2021-07-29 22:36:34 +0530

    [LOGIN] add signup bottom

--- lib/presentation/screen/login/widget_bottom_signup.dart
+++ lib/presentation/screen/login/widget_bottom_signup.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:myseat/utils/my_const/my_const.dart';
 
 class WidgetBottomSignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(top: 24,bottom: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            flex: 2,
+            child: Text(
+              'Don\'t have an account ?',
+              style: FONT_CONST.REGULAR_WHITE_10,
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                'Sign up',
+                style: FONT_CONST.SEMIBOLD_WHITE_10.copyWith(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Text('Here', style: FONT_CONST.SEMIBOLD_WHITE_10),
+          ),
+        ],
+      ),
+    );
   }
 }
