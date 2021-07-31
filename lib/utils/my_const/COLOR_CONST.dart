64a345fef6487f73e6f051a40d241fc7c2adf8ad
satyamx64 <satyamx64@gmail.com>
2021-07-31 12:32:24 +0530

    [SHOW INFO] add widget offers

--- lib/utils/my_const/COLOR_CONST.dart
+++ lib/utils/my_const/COLOR_CONST.dart
@@ -4,7 +4,9 @@ import 'package:findseat/presentation/custom_ui/custom_ui.dart';
 class COLOR_CONST {
   static final DEFAULT = HexColor("#E81667");
   static final RED = HexColor("#F93F3E");
+  static final RED2 = HexColor("#D05045");
   static final GREEN = HexColor("#2EC492");
+  static final GREEN2 = HexColor("#8CC153");
   static final ORANGE = HexColor("#EB8D2F");
   static final WHITE = HexColor("#FFFFFF");
   static final WHITE_70 = HexColor("#B3FFFFFF");
@@ -18,6 +20,7 @@ class COLOR_CONST {
   static final GRAY4 = HexColor("#666666");
   static final GRAY5 = HexColor("#C1C1C1");
   static final GRAY6 = HexColor("#707070");
+  static final GRAY7 = HexColor("#DDDDDD");
   static final ITEM_BG = HexColor("#F3F6F8");
   static final BLUE = HexColor("#222539");
 
@@ -26,6 +29,8 @@ class COLOR_CONST {
   static final FACEBOOK_BTN = HexColor("#F5F9FF");
   static final FACEBOOK_BORDER_BTN = HexColor("#3164CE");
   static final TAG_BG = HexColor("#FFE5EF");
-  
+  static final GIFT_BG1 = HexColor("#FCCAC6");
+  static final GIFT_BG2 = HexColor("#2687D57E");
+
   static final STATUS_BAR = MaterialColor(0xFF2E3147, {});
 }
