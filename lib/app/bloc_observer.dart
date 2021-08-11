4749b7d90fbb99ce80a0ce601c16cf0150e64d21
satyamx64 <satyamx64@gmail.com>
2021-08-11 14:10:08 +0530

    format bloc delegate data

--- lib/app/bloc_observer.dart
+++ lib/app/bloc_observer.dart
@@ -1,16 +1,18 @@
 import 'package:bloc/bloc.dart';
+import 'package:intl/intl.dart';
 
 class MyBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    print(event);
+    log('onEvent', event!);
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stacktrace) {
     super.onError(bloc, error, stacktrace);
     print(error);
+    log('onError', error);
   }
 
   @override
@@ -22,6 +24,15 @@ class MyBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    log(
+        'onTransition',
+        '\tcurrentState=${transition.currentState}\n'
+            '\tnextState=${transition.nextState}');
+  }
+
+  void log(String name, Object msg) {
+    print(
+        '===== ${DateFormat("HH:mm:ss-dd MMM, yyyy").format(DateTime.now())}: $name\n'
+        '$msg');
   }
 }
